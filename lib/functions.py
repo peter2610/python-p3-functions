5334f4806468f86e558a07ebc23ffdca879ec4d4
peter munyambu <petermunyambu1000@gmail.com>
2025-08-30 12:08:11 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+def greet_programmer():
+    print("Hello, programmer!")
 
 def greet_programmer():
     pass
@@ -14,3 +16,19 @@ def add(num1, num2):
 
 def halve(number):
     pass
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+def add(num1, num2):
+    return num1 + num2
+
+def halve(number):
+    return number / 2
